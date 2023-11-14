4319104dfa0ba6fec3ec3fa6a07bb980378c97cb
Duilio Tomas Sargentoni Audisio <89033@sistemas.frc.utn.edu.ar>
2023-11-14 17:08:09 -0300

    ultima act

--- tpi-backend-main/Alquileres/alquileres/src/main/java/utn/backend/grupo128/alquileres/repositories/AlquilerRepository.java
+++
@@ -1,7 +0,0 @@
-package utn.backend.grupo128.alquileres.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import utn.backend.grupo128.alquileres.models.Alquiler;
-
-public interface AlquilerRepository extends JpaRepository<Alquiler, Long> {
-}
--- tpi-backend-main/Alquileres/alquileres/src/main/java/utn/backend/grupo128/alquileres/repositories/TarifaRepository.java
+++
@@ -1,9 +0,0 @@
-package utn.backend.grupo128.alquileres.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import utn.backend.grupo128.alquileres.models.Tarifa;
-
-public interface TarifaRepository extends JpaRepository<Tarifa, Long> {
-
-
-}
