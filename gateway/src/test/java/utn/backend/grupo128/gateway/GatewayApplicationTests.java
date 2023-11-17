b18c0c67fc31854b5e08932980281a1db4dc33cc
Tomi Malamud <tomasmalamud@gmail.com>
2023-11-17 14:40:55 -0300

    api gateway - CalculadorDistancia

---
+++ gateway/src/test/java/utn/backend/grupo128/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package utn.backend.grupo128.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
