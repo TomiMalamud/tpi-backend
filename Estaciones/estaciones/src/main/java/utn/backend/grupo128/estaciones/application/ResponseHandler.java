a77cbad745156c310a8a05ce462a6c27f51a4d3f
Tomi Malamud <tomasmalamud@gmail.com>
2023-11-21 00:59:25 -0300

    DTOs y Keycloak

--- Estaciones/estaciones/src/main/java/utn/backend/grupo128/estaciones/application/ResponseHandler.java
+++ Estaciones/estaciones/src/main/java/utn/backend/grupo128/estaciones/application/ResponseHandler.java
@@ -9,7 +9,6 @@ import java.util.Map;
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
 
 public class ResponseHandler {
-
     public static ResponseEntity<Object> generateResponse(String message, HttpStatus status, Object responseObj) {
         Map<String, Object> response = new HashMap<>();
         response.put("status", status.value());
@@ -23,20 +22,12 @@ public class ResponseHandler {
         return new ResponseEntity<>(response, status);
     }
 
-    public static ResponseEntity<Object> success() {
-        return generateResponse(null, HttpStatus.OK, null);
-    }
-
     public static ResponseEntity<Object> notFound() {
         return generateResponse(null, HttpStatus.NOT_FOUND, null);
     }
 
     public static ResponseEntity<Object> success(Object responseObj) {
         return generateResponse(null, HttpStatus.OK, responseObj);
-    }
-
-    public static ResponseEntity<Object> created(Object responseObj) {
-        return generateResponse(null, HttpStatus.CREATED, responseObj);
     }
 
     public static ResponseEntity<Object> badRequest(String message) {
@@ -46,12 +37,9 @@ public class ResponseHandler {
     public static ResponseEntity<Object> noContent() {
         return generateResponse(null, HttpStatus.NO_CONTENT, null);
     }
+
     public static ResponseEntity<Object> internalError() {
         return generateResponse(null, HttpStatus.INTERNAL_SERVER_ERROR, null);
     }
 
-    public static ResponseEntity<Object> unauthorized() {
-        return generateResponse(null, HttpStatus.UNAUTHORIZED, null);
-    }
 }
-
--- gateway/src/main/java/utn/backend/grupo128/gateway/GWConfig.java
+++ gateway/src/main/java/utn/backend/grupo128/gateway/GWConfig.java
@@ -5,8 +5,17 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtGrantedAuthoritiesConverterAdapter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
+@EnableWebFluxSecurity
 public class GWConfig {
 
     @Bean
@@ -22,4 +31,35 @@ public class GWConfig {
                 .route(p -> p.path("/api/tarifas/**").uri(uriAlquileres))
                 .build();
     }
+
+    @Bean
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) throws
+            Exception {
+        http.authorizeExchange(exchanges -> exchanges
+
+                                .pathMatchers("/api/alquileres/**")
+                                .hasRole("CLIENTE")
+                                .pathMatchers("/api/estaciones/**")
+                                .hasRole("CLIENTE")
+                                .anyExchange()
+                                .authenticated()
+                ).oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtDecoder(jwtDecoder())))
+                .csrf(ServerHttpSecurity.CsrfSpec::disable);
+        return http.build();
+    }
+    @Bean
+    public ReactiveJwtAuthenticationConverter jwtAuthenticationConverter() {
+        var jwtAuthenticationConverter = new ReactiveJwtAuthenticationConverter();
+        var grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        grantedAuthoritiesConverter.setAuthoritiesClaimName("authorities");
+        grantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new ReactiveJwtGrantedAuthoritiesConverterAdapter(grantedAuthoritiesConverter));
+        return jwtAuthenticationConverter;
+    }
+    @Bean
+    public ReactiveJwtDecoder jwtDecoder() {
+        String jwkSetUri = "https://labsys.frc.utn.edu.ar/aim/realms/backend-tps/protocol/openid-connect/certs";
+        return NimbusReactiveJwtDecoder.withJwkSetUri(jwkSetUri).build();
+    }
+
 }
